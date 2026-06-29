f09c225df79e706a0fb9fe6394c188388b2aa5b5
bbwdadfg <baiwei.chu@gmail.com>
2026-06-29 17:04:27 +0800

    Fix Ansible Galaxy import metadata

--- ansible_collections/bbwdadfg/m3u8_player_site_kit/plugins/modules/links.py
+++ ansible_collections/bbwdadfg/m3u8_player_site_kit/plugins/modules/links.py
@@ -2,6 +2,35 @@
 
 from ansible.module_utils.basic import AnsibleModule
 
+DOCUMENTATION = r'''
+---
+module: links
+short_description: Return M3U8 Player product links
+version_added: "0.1.0"
+description:
+  - Returns stable metadata links for M3U8 Player online M3U8, HLS, and IPTV stream tools.
+options: {}
+author:
+  - M3U8 Player
+'''
+
+EXAMPLES = r'''
+- name: Get M3U8 Player links
+  bbwdadfg.m3u8_player_site_kit.links:
+  register: m3u8_player
+'''
+
+RETURN = r'''
+homepage:
+  description: M3U8 Player homepage.
+  returned: always
+  type: str
+links:
+  description: Product links for online M3U8 player, downloader, converter, HLS player, and IPTV player.
+  returned: always
+  type: dict
+'''
+
 
 def main():
     module = AnsibleModule(argument_spec={}, supports_check_mode=True)
